5bde5bd78cd9d1cd1acfcb83d76a66e96ff0fd22
John Caldwell <caldwelljs@gmail.com>
2013-11-26 15:31:48 -0600

    create twitte application to compare number of twitter followers

--- follower-clash.rb
+++ follower-clash.rb
@@ -1,1 +1,13 @@
 require_relative 'lib/user_comparer'
+
+mike = UserComparer::User.new('ornellasmike')
+puts mike
+
+matt = UserComparer::User.new('techpeace')
+puts matt
+
+puts "#{mike.username}'s follower count: #{mike.followers}"
+puts "#{matt.username}'s follower count: #{matt.followers}"
+
+new_comparison = UserComparer::Comparer.new(mike, matt)
+new_comparison.compare(mike, matt)
--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -4,15 +4,39 @@ require 'twitter'
 
 module UserComparer
 	class User
+		attr_reader :username
+
+		def initialize(username)
+			@username = username
+		end
+
+		def followers
+			client = Twitter::REST::Client.new do |config|
+				config.consumer_key 		    = ENV['CONSUMER_KEY']
+				config.consumer_secret     = ENV['CONSUMER_SECRET']
+				config.access_token			    = ENV['OAUTH_TOKEN']
+				config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+			end
+
+			client.user(@username).followers_count
+		end
 	end
 
 	class Comparer
+		
 		def initialize(user1, user2)
 			@user1 = user1
 			@user2 = user2
 		end
 
-		def compare
+		def compare(user1, user2)
+			if user1.followers > user2.followers
+				puts user1.username
+			elsif user1.followers < user2.followers
+				puts user2.username
+			else
+				puts "Users have the same number of followers"
+			end
 		end
 	end
 end
