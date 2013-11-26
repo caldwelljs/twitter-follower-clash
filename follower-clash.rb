aa68742b671f68e8172b4a0e2c4d7c934056ded9
John Caldwell <caldwelljs@gmail.com>
2013-11-26 16:24:08 -0600

    add friend-count comparisons and tweet-count comparisons

--- follower-clash.rb
+++ follower-clash.rb
@@ -1,13 +1,26 @@
 require_relative 'lib/user_comparer'
 
-mike = UserComparer::User.new('ornellasmike')
-puts mike
+john = UserComparer::User.new('caldwelljs')
+puts john
 
-matt = UserComparer::User.new('techpeace')
-puts matt
+kendra = UserComparer::User.new('kendram23')
+puts kendra
 
-puts "#{mike.username}'s follower count: #{mike.followers}"
-puts "#{matt.username}'s follower count: #{matt.followers}"
+puts "#{john.username}'s follower count: #{john.followers}"
+puts "#{kendra.username}'s follower count: #{kendra.followers}"
 
-new_comparison = UserComparer::Comparer.new(mike, matt)
-new_comparison.compare(mike, matt)
+new_comparison = UserComparer::Comparer.new(john, kendra)
+new_comparison.compare_followers(john, kendra)
+
+puts "#{john.username}'s friend count: #{john.friends}"
+puts "#{kendra.username}'s friend count: #{kendra.friends}"
+
+new_comparison.compare_friend_count(john, kendra)
+
+
+new_comparison = UserComparer::Comparer.new(john, kendra)
+new_comparison.compare_tweet_count(john, kendra)\
+
+puts "#{john.username}'s tweet count: #{john.tweet_counter}"
+puts "#{kendra.username}'s tweet count: #{kendra.tweet_counter}"
+
--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -10,15 +10,26 @@ module UserComparer
 			@username = username
 		end
 
-		def followers
+		def auth
 			client = Twitter::REST::Client.new do |config|
 				config.consumer_key 		    = ENV['CONSUMER_KEY']
 				config.consumer_secret     = ENV['CONSUMER_SECRET']
 				config.access_token			    = ENV['OAUTH_TOKEN']
 				config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
 			end
+			client
+		end
 
-			client.user(@username).followers_count
+		def followers
+			auth.user(@username).followers_count
+		end
+		
+		def friends
+			auth.user(@username).friends_count
+		end
+
+		def tweet_counter
+			auth.user(@username).tweets_count
 		end
 	end
 
@@ -29,13 +40,33 @@ module UserComparer
 			@user2 = user2
 		end
 
-		def compare(user1, user2)
+		def compare_followers(user1, user2)
 			if user1.followers > user2.followers
-				puts user1.username
+				puts "And the user with the most followers is: #{user1.username}"
 			elsif user1.followers < user2.followers
-				puts user2.username
+				puts "And the user with the most followers is: #{user2.username} "
 			else
 				puts "Users have the same number of followers"
+			end
+		end
+
+		def compare_friend_count(user1, user2)
+			if user1.friends > user2.friends
+				puts "And the user with the most friends is: #{user1.username}"
+			elsif user1.friends < user2.friends
+				puts "And the user with the most friends is: #{user2.username} "
+			else
+				puts "Users have the same number of friends"
+			end
+		end
+
+		def compare_tweet_count(user1, user2)
+			if user1.tweet_counter > user2.tweet_counter
+				puts "And the user with the most tweets is: #{user1.username}"
+			elsif user1.tweet_counter < user2.tweet_counter
+				puts "And the user with the most tweets is: #{user2.username} "
+			else
+				puts "Users have the same number of friends"
 			end
 		end
 	end
